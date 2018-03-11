e955d8c96845c3d2cd067ef7b185afafc19bd9e9
Natalia Bajek <nataliabajek92@gmail.com>
2018-03-11 19:16:20 +0100

    Table and events without attributes

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase';
 
 const app = initializeApp({
-  apiKey: 'AIzaSyAs4yqbLgAjf8jkCsW8Mp-DSVdMvPUBrHc',
-  authDomain: 'vueapp-16eb6.firebaseapp.com',
-  databaseURL: 'https://vueapp-16eb6.firebaseio.com',
-  projectId: 'vueapp-16eb6',
-  storageBucket: 'vueapp-16eb6.appspot.com',
-  messagingSenderId: '388866802369'
+	apiKey: 'AIzaSyAs4yqbLgAjf8jkCsW8Mp-DSVdMvPUBrHc',
+	authDomain: 'vueapp-16eb6.firebaseapp.com',
+	databaseURL: 'https://vueapp-16eb6.firebaseio.com',
+	projectId: 'vueapp-16eb6',
+	storageBucket: 'vueapp-16eb6.appspot.com',
+	messagingSenderId: '388866802369'
 });
 
 export const db = app.database();
--- src/AppDark.vue
+++ src/AppDark.vue
@@ -1,47 +1,71 @@
+<style scoped>
+    .products{
+        background-color: #424242;
+    }
+    button {
+        padding: 5px 8px;
+    }
+    .cart-btn {
+        background-color: firebrick;
+    }
+    .wishlist-btn {
+        background-color: dodgerblue;
+    }
+    .price {
+        padding: 5px 8px;
+    }
+    .special {
+        background-color: yellow;
+        color: black;
+    }
+</style>
+
 <template>
 
 <v-app id="inspire" dark>
-  <v-navigation-drawer clipped fixed v-model="drawer" app>
-    <v-list dense>
-      <v-list-tile v-for="(item, i) in items" :key="i" @click="navigateTo(item.target)">
-        <v-list-tile-action>
-          <v-icon v-html="item.icon"></v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title v-text="item.title"></v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
-  <v-toolbar app fixed clipped-left>
-    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-    <v-toolbar-title>Bold FE hackathon#1</v-toolbar-title>
-  </v-toolbar>
-  <v-content>
-    <v-container fluid fill-height>
-      <v-layout justify-center align-center>
-      	<table>
-      		<tr>
-      			<th>Minicart</th>
-      			<td v-for="beer in products" v-bind:key="beer['.key']">
-	      			<img v-bind:src="'../static/products/' + beer.image">
-				      <p>{{beer.name}}</p> 
-				      <p>{{beer.price.value}}</p>
-				      <button>Koszyk</button>
-				      <button>Wishlista</button>
-      			</td>
-      		</tr>
-      		<tr v-for="attrName in attributes" v-bind:key="attrName['.key']">
-      			<th>{{attrName.name}}</th>
-      			<td v-for="beer in products" v-bind:key="beer['.key']">{{beer.attributes[0]}}</td>
-      		</tr>
-      	</table>
-      </v-layout>
-    </v-container>
-  </v-content>
-  <v-footer app fixed>
-    <span>&copy; 2018</span>
-  </v-footer>
+    <v-navigation-drawer clipped fixed v-model="drawer" app>
+        <v-list dense>
+            <v-list-tile v-for="(item, i) in items" :key="i" @click="navigateTo(item.target)">
+                <v-list-tile-action>
+                    <v-icon v-html="item.icon"></v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Bold FE hackathon#1</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+        <v-container fluid fill-height>
+            <v-layout justify-center align-center>
+                <table>
+                    <tr>
+                        <th>Minicart</th>
+                        <td v-for="beer in products" v-bind:key="beer['.key']" class="products">
+                            <img v-bind:src="'../static/products/' + beer.image">
+                            <p>{{beer.name}}</p> 
+                            <p class="price" :class="beer.price.type">{{beer.price.value}}</p>
+                            <button class="cart-btn" v-on:click="addToCart">Koszyk</button>
+                            <button class="wishlist-btn" v-on:click="addToWishlist">Wishlista</button>
+                        </td>
+                    </tr>
+                    <tr v-for="(attName, attNameIndex) in attributes" v-bind:key="attName['.key']">
+                        <th>{{attName.name}}</th>
+                        <td v-for="beer in products" v-bind:key="beer['.key']">
+                          {{beer.attributes[attNameIndex]}}
+                        </td>
+                    </tr>
+                </table>
+            </v-layout>
+        </v-container>
+    </v-content>
+    <v-footer app fixed>
+        <span>&copy; 2018</span>
+    </v-footer>
 </v-app>
 
 </template>
@@ -52,30 +76,48 @@ import { products } from '../config/firebase.js'
 import { attributes } from '../config/firebase.js'
 import { attributesIdValue } from '../config/firebase.js'
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: Menu,
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+    data () {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: false,
+            items: Menu,
+            miniVariant: false,
+            right: true,
+            rightDrawer: false,
+            title: 'Vuetify.js',
+            counter: 0,
+            cart: [],
+            wishlist: []
+        }
+    },
+    methods: {
+        navigateTo (nav) {
+            this.$router.push({
+                path: nav
+            })
+        },
+        addToCart () {
+            this.cart.push(this.counter)
+            this.counter++
+            console.log(this.cart)
+        },
+        addToWishlist () {
+            this.wishlist.push({name: this.id})
+        }
+    },
+    name: 'AppDark',
+    firebase: {
+        products: products,
+        attributes: attributes,
+        attributesIdValue: attributesIdValue
     }
-  },
-  methods: {
-    navigateTo (nav) {
-      this.$router.push({
-        path: nav
-      })
-    }
-  },
-  name: 'AppDark',
-	  firebase: {
-  	products: products,
-  	attributes: attributes,
-  	attributesIdValue: attributesIdValue
-  }
 }
+    // attributesIdValue = function(){
+    //    var result = [];
+    //    for(var i=0; i<attributesIdValue.length; i++){
+    //        result.push(attributesIdValue[i].split("|"))
+    //    }
+    //    return result
+    //   }
 </script>
